2f5b191b23b1bfb1831c9fd0406cea416f101727
Steven Bayer <smbayer@amazon.com>
2025-02-03 12:03:37 -0600

    lvim config snapshot before claud
    Signed-off-by: Steven Bayer <smbayer@amazon.com>

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -1,8 +1,3 @@
--- Read the docs: https://www.lunarvim.org/docs/configuration
--- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
--- Forum: https://www.reddit.com/r/lunarvim/
--- Discord: https://discord.com/invite/Xb9B4Ny
-
 vim.opt.relativenumber = true
 
 -- use treesitter folding
@@ -12,6 +7,8 @@ vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 -- File explorer
 lvim.keys.normal_mode["<C-b>"] = false
 lvim.keys.normal_mode["<C-b>"] = ":NvimTreeToggle<CR>"
+lvim.keys.normal_mode["<C-1>"] = ":NvimTreeToggle<CR>"
+-- vim.api.nvim_set_keymap("n", "<D-1>", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
 
 -- Indent / Unindent
 -- Conflicts with code folding
@@ -26,4 +23,21 @@ lvim.keys.normal_mode["<LEADER>fh"] = "<CMD>lua require('telescope').help_tags()
 
 -- ToggleTerm
 lvim.keys.normal_mode["<C-`>"] = ":ToggleTerm<CR>"
+
+-- Diable code folding
+vim.wo.foldenable = false
+
+-- Override indent level
+lvim.keys.normal_mode["<C-t>"] = function()
+    local current_indent = vim.opt_local.shiftwidth:get()
+    if current_indent == 4 then
+        vim.opt_local.tabstop = 2
+        vim.opt_local.shiftwidth = 2
+        vim.opt_local.softtabstop = 2
+    else
+        vim.opt_local.tabstop = 4
+        vim.opt_local.shiftwidth = 4
+        vim.opt_local.softtabstop = 4
+    end
+end
 
