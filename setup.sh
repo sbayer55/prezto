8e8fca1ea774860d30d4cb4e7bcd3d159800d8e2
Steven Bayer <sbayer55@gmail.com>
2026-06-18 16:35:56 -0500

    Remove oh my zsh and p10k

--- setup.sh
+++ setup.sh
@@ -42,24 +42,10 @@ eval "${ZPREZTO_HOME}/scripts/install-dev-tools.sh"
 
 git clone --depth 1 https://github.com/junegunn/fzf.git "${HOME}/.fzf"
 
-ZSH_CUSTOM="${HOME}/.zprezto/oh-my-zsh/custom"
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
-git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
-
 echo -e "${YELLOW}Install Starship? [Y/n]${NC}"
 read -r input
 
 if [[ -z "$input" || "$input" =~ ^[Yy]$ ]]; then
     # Need OS check
     curl -sS https://starship.rs/install.sh | shi
-fi
-
-echo -e "${YELLOW}Install Oh My ZSH? [Y/n]${NC}"
-read -r input
-
-if [[ -z "$input" || "$input" =~ ^[Yy]$ ]]; then
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
